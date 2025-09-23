1699ccd1b91359f90e63c9f9db50a2f04c821bf9
adam <adamsafri05@gmail.com>
2025-09-23 23:17:34 +0800

    Function to get music recommendation based on genre

--- Program.cpp
+++ Program.cpp
@@ -15,3 +15,21 @@ void displayMainMenu() {
     cout << "3. Exit" << endl;
     cout << "Enter your choice: ";
 }
+
+// Function to get music recommendation based on genre
+void getMusicRecommendation() {
+    cout << "\n--- MUSIC RECOMMENDATION ---" << endl;
+    cout << "Choose your favorite genre:" << endl;
+    cout << "1. Country Music" << endl;
+    cout << "2. Rock and Roll" << endl;
+    cout << "3. Reggae" << endl;
+    cout << "4. Popular Music" << endl;
+    cout << "5. Indie Rock" << endl;
+    cout << "6. Latina" << endl;
+    cout << "7. Pop Rock" << endl;
+    cout << "8. Alternative R&B" << endl;
+    cout << "9. Alternative Rock" << endl;
+    cout << "Enter genre choice (1-9): ";
+    
+    int genreChoice;
+    cin >> genreChoice;
